8298041cde1fa28ae1e9108f3590223dde2ddc92
JingYee-JY <jingyeetan2005@gmail.com>
2022-10-20 12:39:52 +0800

    Update

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ const board = document.getElementById("board")
 const piecesCon = document.getElementById("pieces")
 const playAgain = document.querySelector(".againButton")
 const final = document.querySelector(".final");
+const pop = document.querySelector(".pop");
+const homeButton = document.querySelector(".homeButton")
+
+const clickSound = document.getElementById("click")
+const clap = document.getElementById("clap")
+const completed = document.getElementById("completed")
+const lose = document.getElementById("lose")
 
 let rows;
 let columns;
-let endpuzzle;
+let questionButton;
 
-var checkP1
-var checkP2
-var checkP3
-
+let chosenAnswer;
 let rounds;
 let totalRound;
 
@@ -30,17 +34,17 @@ puzzle1Easy = [
 ]
 
 puzzle2Easy = [
-    {number: 1, image: "./img/Puzzle 2_E1.png"},
-    {number: 2, image: "./img/Puzzle 2_E2.png"},
-    {number: 3, image: "./img/Puzzle 2_E3.png"},
-    {number: 4, image: "./img/Puzzle 2_E4.png"}
+    {number: 4, image: "./img/Puzzle 2_E1.png"},
+    {number: 5, image: "./img/Puzzle 2_E2.png"},
+    {number: 6, image: "./img/Puzzle 2_E3.png"},
+    {number: 7, image: "./img/Puzzle 2_E4.png"}
 ]
 
 puzzle3Easy = [
-    {number: 1, image: "./img/Puzzle 3_E1.png"},
-    {number: 2, image: "./img/Puzzle 3_E2.png"},
-    {number: 3, image: "./img/Puzzle 3_E3.png"},
-    {number: 4, image: "./img/Puzzle 3_E4.png"}
+    {number: 8, image: "./img/Puzzle 3_E1.png"},
+    {number: 9, image: "./img/Puzzle 3_E2.png"},
+    {number: 10, image: "./img/Puzzle 3_E3.png"},
+    {number: 11, image: "./img/Puzzle 3_E4.png"}
 ]
 
 puzzle1Normal = [
@@ -53,21 +57,21 @@ puzzle1Normal = [
 ]
 
 puzzle2Normal = [
-    {number: 1, image: "./img/Puzzle 2_N1.png"},
-    {number: 2, image: "./img/Puzzle 2_N2.png"},
-    {number: 3, image: "./img/Puzzle 2_N3.png"},
-    {number: 4, image: "./img/Puzzle 2_N4.png"},
-    {number: 5, image: "./img/Puzzle 2_N5.png"},
-    {number: 6, image: "./img/Puzzle 2_N6.png"}
+    {number: 7, image: "./img/Puzzle 2_N1.png"},
+    {number: 8, image: "./img/Puzzle 2_N2.png"},
+    {number: 9, image: "./img/Puzzle 2_N3.png"},
+    {number: 10, image: "./img/Puzzle 2_N4.png"},
+    {number: 11, image: "./img/Puzzle 2_N5.png"},
+    {number: 12, image: "./img/Puzzle 2_N6.png"}
 ]
 
 puzzle3Normal = [
-    {number: 1, image: "./img/Puzzle 3_N1.png"},
-    {number: 2, image: "./img/Puzzle 3_N2.png"},
-    {number: 3, image: "./img/Puzzle 3_N3.png"},
-    {number: 4, image: "./img/Puzzle 3_N4.png"},
-    {number: 5, image: "./img/Puzzle 3_N5.png"},
-    {number: 6, image: "./img/Puzzle 3_N6.png"}
+    {number: 13, image: "./img/Puzzle 3_N1.png"},
+    {number: 14, image: "./img/Puzzle 3_N2.png"},
+    {number: 15, image: "./img/Puzzle 3_N3.png"},
+    {number: 16, image: "./img/Puzzle 3_N4.png"},
+    {number: 17, image: "./img/Puzzle 3_N5.png"},
+    {number: 18, image: "./img/Puzzle 3_N6.png"}
 ]
 
 puzzle1Hard = [
@@ -83,120 +87,147 @@ puzzle1Hard = [
 ]
 
 puzzle2Hard = [
-    {number: 1, image: "./img/Puzzle 2_H1.png"},
-    {number: 2, image: "./img/Puzzle 2_H2.png"},
-    {number: 3, image: "./img/Puzzle 2_H3.png"},
-    {number: 4, image: "./img/Puzzle 2_H4.png"},
-    {number: 5, image: "./img/Puzzle 2_H5.png"},
-    {number: 6, image: "./img/Puzzle 2_H6.png"},
-    {number: 7, image: "./img/Puzzle 2_H7.png"},
-    {number: 8, image: "./img/Puzzle 2_H8.png"},
-    {number: 9, image: "./img/Puzzle 2_H9.png"}
+    {number: 10, image: "./img/Puzzle 2_H1.png"},
+    {number: 11, image: "./img/Puzzle 2_H2.png"},
+    {number: 12, image: "./img/Puzzle 2_H3.png"},
+    {number: 13, image: "./img/Puzzle 2_H4.png"},
+    {number: 14, image: "./img/Puzzle 2_H5.png"},
+    {number: 15, image: "./img/Puzzle 2_H6.png"},
+    {number: 16, image: "./img/Puzzle 2_H7.png"},
+    {number: 17, image: "./img/Puzzle 2_H8.png"},
+    {number: 18, image: "./img/Puzzle 2_H9.png"}
 ]
 
 puzzle3Hard = [
-    {number: 1, image: "./img/Puzzle 3_H1.png"},
-    {number: 2, image: "./img/Puzzle 3_H2.png"},
-    {number: 3, image: "./img/Puzzle 3_H3.png"},
-    {number: 4, image: "./img/Puzzle 3_H4.png"},
-    {number: 5, image: "./img/Puzzle 3_H5.png"},
-    {number: 6, image: "./img/Puzzle 3_H6.png"},
-    {number: 7, image: "./img/Puzzle 3_H7.png"},
-    {number: 8, image: "./img/Puzzle 3_H8.png"},
-    {number: 9, image: "./img/Puzzle 3_H9.png"}
+    {number: 19, image: "./img/Puzzle 3_H1.png"},
+    {number: 20, image: "./img/Puzzle 3_H2.png"},
+    {number: 21, image: "./img/Puzzle 3_H3.png"},
+    {number: 22, image: "./img/Puzzle 3_H4.png"},
+    {number: 23, image: "./img/Puzzle 3_H5.png"},
+    {number: 24, image: "./img/Puzzle 3_H6.png"},
+    {number: 25, image: "./img/Puzzle 3_H7.png"},
+    {number: 26, image: "./img/Puzzle 3_H8.png"},
+    {number: 27, image: "./img/Puzzle 3_H9.png"}
 ]
 
 startButton.addEventListener("click", () => {
-    start.classList.add("hide")
-    selection.classList.remove("hide")
-    totalRound = 3
-    rounds = 0
+    playClickSound()
+    let delay = setTimeout(() => {
+        start.classList.add("hide")
+        selection.classList.remove("hide")
+        totalRound = 3
+        rounds = 0
+    }, 200);
 })
 
 easy.addEventListener("click", () => {
-    selection.classList.add("hide")
-    game.classList.remove("hide")
-    rows = 2
-    columns =  2
-    puzzle = 4
-    puzzleSelected = null
-    puzzleNumber = null
-    Spawn()
+    playClickSound()
+    let delay = setTimeout(() => {
+        selection.classList.add("hide")
+        game.classList.remove("hide")
+        rows = 2
+        columns =  2
+        puzzle = 4
+        puzzleSelected = null
+        puzzleNumber = null
+        Spawn()
+    }, 200);
 })
 
 normal.addEventListener("click", () => {
-    selection.classList.add("hide")
-    game.classList.remove("hide")
-    rows = 2
-    columns =  3
-    puzzle = 6
-    puzzleSelected = null
-    puzzleNumber = null
-    Spawn()
+    let delay = setTimeout(() => {
+        selection.classList.add("hide")
+        game.classList.remove("hide")
+        rows = 2
+        columns =  3
+        puzzle = 6
+        puzzleSelected = null
+        puzzleNumber = null
+        Spawn()
+    }, 200);
 })
 
 hard.addEventListener("click", () => {
-    selection.classList.add("hide")
-    game.classList.remove("hide")
-    rows = 3
-    columns =  3
-    puzzle = 9
-    puzzleSelected = null
-    puzzleNumber = null
-    Spawn()
+    let delay = setTimeout(() => {
+        selection.classList.add("hide")
+        game.classList.remove("hide")
+        rows = 3
+        columns =  3
+        puzzle = 9
+        puzzleSelected = null
+        puzzleNumber = null
+        Spawn()
+    }, 200);
 })
 
 playAgain.addEventListener("click", () => {
-    final.classList.add("hide")
-    start.classList.remove("hide")
+    playClickSound()
+    let delay = setTimeout(() => {
+        final.classList.add("hide")
+        start.classList.remove("hide")
+    }, 200);
+})
+
+homeButton.addEventListener("click", () => {
+    playClickSound()
+    let delay = setTimeout(() => {
+      location.assign('https://gimme.sg/activations/dementia/');
+    }, 200);
 })
 
 function Spawn(){
 
     rounds += 1;
+    chosenAnswer = false
 
     round.innerHTML = `<p>${rounds}/${totalRound}</p>`
     let randomPuzzle = Math.floor(Math.random() * 3);
     let puzzlePieces
+    let other
 
     if(puzzle == 4){
         if(randomPuzzle == 0){
             puzzlePieces = puzzle1Easy
+            other = puzzle2Easy
         }
         if(randomPuzzle == 1){
-            puzzlePieces = puzzle2Easy    
+            puzzlePieces = puzzle2Easy   
+            other = puzzle3Easy 
         }
         if(randomPuzzle == 2){
             puzzlePieces = puzzle3Easy
+            other = puzzle1Easy
         }
-        endpuzzle = 1
         board.classList.add("boardP4")
     }
     if(puzzle == 6){
         if(randomPuzzle == 0){
             puzzlePieces = puzzle1Normal
+            other = puzzle2Normal
         }
         if(randomPuzzle == 1){
             puzzlePieces = puzzle2Normal
+            other = puzzle3Normal
         }
         if(randomPuzzle == 2){
             puzzlePieces = puzzle3Normal
+            other = puzzle1Normal
         }
-        endpuzzle = 2
         board.classList.add("boardP6")
     }
     if(puzzle == 9){
         if(randomPuzzle == 0){
             puzzlePieces = puzzle1Hard
+            other = puzzle2Hard
         }
         if(randomPuzzle == 1){
             puzzlePieces = puzzle2Hard
-            
+            other = puzzle3Hard
         }
         if(randomPuzzle == 2){
             puzzlePieces = puzzle3Hard
+            other = puzzle1Hard
         }
-        endpuzzle = 3
         board.classList.add("boardP9")
     }
 
@@ -215,29 +246,13 @@ function Spawn(){
             tile.setAttribute("data", puzzlePieces[i].number)
 
             board.append(tile)
-            
-            let asignButton = document.querySelector(`.${currentClass}`)
-            let number = tile.getAttribute("data")
-            
-            asignButton.addEventListener("click", () => {
-                if(puzzleNumber == null & !asignButton.classList.contains("question")) {return}
-                if(puzzleNumber == number & asignButton.classList.contains("question")){
-                    puzzleSelected.classList.add("right")
-                    asignButton.style.opacity = "1";
-                    asignButton.classList.add("done")
-                    Check()
-                }
-                puzzleSelected.style.border = ""
-                puzzleSelected = null
-                puzzleNumber = null
-            })
         }
     }
 
     //pieces
     let pieces = [];
     for(let i = 1; i <= rows*columns; i++){
-        pieces = [...puzzlePieces]
+        pieces = [...other]
     }
 
     pieces.reverse();
@@ -269,24 +284,35 @@ function Spawn(){
 
         asignButton.addEventListener("click", () => {
             let puzzleNo = tile.getAttribute("data")
-                if(puzzleSelected == currentClass){
-                    puzzleSelected = null
-                    puzzleNumber = null
-                    asignButton.style.border = ""
+            let answer = questionButton.getAttribute("data")
+            console.log(puzzleNo, answer,questionButton)
+                if(puzzleNo == answer && chosenAnswer == false){
+                    playClickSound()
+                    chosenAnswer = true
+                    completed.currentTime = 0
+                    completed.play()
+                    console.log("S")
+                    questionButton.src = asignButton.src
+                    asignButton.style.opacity = "0"
+                    let delay = setTimeout(() => {
+                        Check()
+                    }, 500);
                     return
                 }
-                else{
-                    if(puzzleSelected != null){
-                        puzzleSelected.style.border = ""
-                    }
-                    puzzleSelected = asignButton
-                    puzzleNumber = puzzleNo
-                    asignButton.style.border = "solid 5px black"
+                if(chosenAnswer == false && puzzleNo !=answer){
+                    playClickSound()
+                    lose.currentTime = 0
+                    lose.play()
+                    pop.classList.remove("hide")
+                    chosenAnswer = true
+                    let delay = setTimeout(() => {
+                        pop.classList.add("hide")
+                        chosenAnswer = false
+                    },1000)
                 }
         })
     }
-
-    for(let o = 0; o < endpuzzle; o++){
+    
         let value = (Math.floor(Math.random() * puzzlePieces.length) + 1)
         let puzzleValue = (Math.floor(Math.random() * 3) + 1)
 
@@ -297,69 +323,32 @@ function Spawn(){
         let answerButton = document.querySelector(`.${answerPuzzle}`)
         
         let getQuestionData = questionButton.getAttribute("data")
-        console.log(questionButton,o)
-        console.log(checkP1, checkP2,checkP3)
-        if(questionButton.classList.contains("question") || answerButton.classList.contains("change")){
-            o--
-            console.log("T")
-        }
-        if(!questionButton.classList.contains("question")  && !answerButton.classList.contains("change")){
-            questionButton.classList.add("question")
-            if(checkP1 == getQuestionData){
-                let btn = document.querySelector(".puzzle1")
-                btn.classList.add("change")
-                console.log(btn, "1")
-            }
-            if(checkP2 == getQuestionData){
-                let btn = document.querySelector(".puzzle2")
-                btn.classList.add("change")
-                console.log(btn, "2")
-            }
-            if(checkP3 == getQuestionData){
-                let btn = document.querySelector(".puzzle3")
-                btn.classList.add("change")
-                console.log(btn, "3")
-            }
-            if(checkP1 != getQuestionData && checkP2 != getQuestionData && checkP3 != getQuestionData){
-            answerButton.classList.add("change")
+            
             let image = questionButton.src
-
             answerButton.src = image
-            if(puzzleValue == 1){
-                checkP1 = parseInt(getQuestionData)
-            }
-            if(puzzleValue == 2){
-                checkP2 = parseInt(getQuestionData)
-            }
-            if(puzzleValue == 3){
-                checkP3 = parseInt(getQuestionData)
-            }
+            questionButton.src = "./img/question.png";
+
             answerButton.setAttribute("data", getQuestionData)
-            console.log(answerButton, "c")
-           }
-        }
-    }
 
 }
 
 function Check(){
-    let correct = document.querySelectorAll(".done")
-        if(correct.length == endpuzzle){
-            if(rounds == totalRound){
-                let delay = setTimeout(() => {
-                    remove()
-                    final.classList.remove("hide")
-                    game.classList.add("hide")
-                }, 500);
-                return
-            }
-            else{
-                let delay = setTimeout(() => {
-                    remove()
-                    Spawn()
-                }, 500);
-            }
+    if(rounds == totalRound){
+        let delay = setTimeout(() => {
+                clap.currentTime = 0
+                clap.play()
+                remove()
+                final.classList.remove("hide")
+                game.classList.add("hide")
+            }, 500);
+            return
         }
+    else{
+        let delay = setTimeout(() => {
+            remove()
+            Spawn()
+        }, 500);
+    }
 }
 
 function remove(){
@@ -378,3 +367,13 @@ function remove(){
         parent.removeChild(parent.firstChild);
     }
 }
+
+function playClickSound(){
+    console.log(clickSound)
+    clickSound.currentTime = 0
+    clickSound.play()
+}
+
+document.addEventListener('dblclick', function(event) {
+event.preventDefault();
+}, { passive: false });
